8e3181a86c4453a1c2860cc71f761425fe4b4c31
tanipu <67771199+yuktnk@users.noreply.github.com>
2021-10-17 19:25:34 +0900

    基本的な型

---
+++ src/typeList.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/* TypeScriptの基本の型 */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "text";
+
+// Array
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "text"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = "test";
+};
+
+// null
+let null1: null = null;
+
+// undefind
+let undefind1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "test" };
