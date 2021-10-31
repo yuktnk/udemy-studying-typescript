34cd68b885a42c1449aa8d8909d623951f49ff8e
tanipu <67771199+yuktnk@users.noreply.github.com>
2021-10-31 16:39:02 +0900

    実践編 axiosの準備とか

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Practice1 } from "./practices/Practice1";
-import { Practice2 } from "./practices/Practice2";
-import { Practice3 } from "./practices/Practice3";
-import { Practice4 } from "./practices/Practice4";
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <div className="App">
-      <Practice1 />
-      <Practice2 />
-      <Practice3 />
-      <Practice4 />
+      <button onClick={onClickFetchData}>データ取得</button>
     </div>
   );
 }
